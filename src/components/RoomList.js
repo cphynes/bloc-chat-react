cefb1a761816a0c5fc4ba3ddcf95ba3553ea6e2b
Christopher Phynes <chrisisnow@mac.com>
2018-03-06 18:30:45 -0500

    bloc-chat-chatrooms: 2nd

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -4,35 +4,31 @@ import React, { Component } from 'react';
 class RoomList extends Component {
   constructor(props) {
     super(props);
-
     this.state = {
       rooms: []
     };
-
     this.roomsRef = this.props.firebase.database().ref('rooms');
   }
 
   componentDidMount() {
-     this.roomsRef.on('child_added', snapshot => {
+    this.roomsRef.on('child_added', snapshot => {
        const room = snapshot.val();
        room.key = snapshot.key;
-       this.setState({ rooms: this.state.rooms.concat( room ) });
+       this.setState({ rooms: this.state.rooms.concat( room ) })
      });
    }
 
-  render() {
-    return (
-      <section className="roomList">
-       <h1>Bloc Chat</h1>
-        <ul>
-        {this.state.rooms.map((room, index) =>
-         <li key={index}>{room.name}</li>
-  )}
-        </ul>
+   render () {
+     return (
+         <div>
+           {
+             this.state.rooms.map( (room, index) =>
+               <li className="room" key={index}>{room.name}</li>
+             )
+           }
+         </div>
+     );
+   }
+ }
 
-
-     </section>
-    );
-  }
-}
 export default RoomList;

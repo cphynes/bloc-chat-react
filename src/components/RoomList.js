5edcc7e9783dbfd0c0fba15b720eb6ad33e6dcfd
Christopher Phynes <chrisisnow@mac.com>
2018-03-09 17:21:22 -0500

    update no t a submission

--- src/components/RoomList.js
+++ src/components/RoomList.js
@@ -14,7 +14,6 @@ class RoomList extends Component {
     this.roomsRef.on('child_added', snapshot => {
        const room = snapshot.val();
        room.key = snapshot.key;
-       console.log(snapshot);
        this.setState({ rooms: this.state.rooms.concat( room ) })
      });
    }
